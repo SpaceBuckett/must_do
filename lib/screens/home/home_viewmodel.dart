b28ee9d9804585b449c30a9bdc48b558a5c8be68
SpaceBuckett <shehxadv2.0@gmail.com>
2022-08-02 23:42:42 +0500

    Now you can add the tasks to your todo list

--- lib/screens/home/home_viewmodel.dart
+++ lib/screens/home/home_viewmodel.dart
@@ -1,6 +1,7 @@
 import 'package:intl/intl.dart';
 import 'package:must_do/core/data/dummy_tasks.dart';
 import 'package:must_do/core/models/base_model.dart';
+import 'package:must_do/core/models/tasks/tasks_model.dart';
 
 class HomeViewModel extends BaseViewModel {
   final DateTime todaysDate = DateTime.now();
@@ -19,6 +20,13 @@ class HomeViewModel extends BaseViewModel {
 
   toggleTask(index) {
     dummyTasks[index].isDone = !dummyTasks[index].isDone;
+    notifyListeners();
+  }
+
+  addTask(String task) {
+    dummyTasks.add(
+      Task(task: task, isDone: false),
+    );
     notifyListeners();
   }
 }
